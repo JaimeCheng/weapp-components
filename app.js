c0d281c92b75d88308a5f7758fb2f227a1acae12
JaimeCheng <403693553@qq.com>
2018-12-28 17:17:27 +0800

    初始化小程序

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 //app.js
 App({
+  globalData: {
+    userInfo: null
+  },
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -32,8 +24,5 @@ App({
         }
       }
     })
-  },
-  globalData: {
-    userInfo: null
   }
 })
